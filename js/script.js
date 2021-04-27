e510ab9f63c722d20177c419329be609efcd7c29
Mirko <mirkosbaglia2001@gmail.com>
2021-04-27 12:46:13 +0200

    Header

---
+++ js/script.js
@@ -0,0 +1,39 @@
+function initVue() {
+    new Vue({
+        el: '#app',
+        data: {
+        navDown:[
+          {
+            link: 'Home',
+          },
+          {
+            link: 'Shop'
+          },
+          {
+            link: 'About'
+          },
+          {
+            link: 'Blog'
+          },
+          {
+            link: 'Contact'
+          },
+          {
+            link: 'Shop by brand'
+          },
+
+        ]
+        },
+        methods: {
+
+        },
+    });
+
+
+}
+
+function init() {
+    initVue();
+
+}
+document.addEventListener('DOMContentLoaded', init);
