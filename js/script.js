6c25406aad5f8224cc06ac2ff674e02d42f7020d
Mirko <mirkosbaglia2001@gmail.com>
2021-04-27 16:56:34 +0200

    4/5 section

--- js/script.js
+++ js/script.js
@@ -2,38 +2,35 @@ function initVue() {
     new Vue({
         el: '#app',
         data: {
-        navDown:[
-          {
-            link: 'Home',
-          },
-          {
-            link: 'Shop'
-          },
-          {
-            link: 'About'
-          },
-          {
-            link: 'Blog'
-          },
-          {
-            link: 'Contact'
-          },
-          {
-            link: 'Shop by brand'
-          },
+          navDown:[
+            {
+              link: 'Home',
+            },
+            {
+              link: 'Shop'
+            },
+            {
+              link: 'About'
+            },
+            {
+              link: 'Blog'
+            },
+            {
+              link: 'Contact'
+            },
+            {
+              link: 'Shop by brand'
+            },
 
-        ]
+          ]
         },
         methods: {
 
         },
+
     });
-
-
 }
-
 function init() {
     initVue();
-
 }
 document.addEventListener('DOMContentLoaded', init);
